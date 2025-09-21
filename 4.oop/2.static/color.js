b8f72d93426e8d890a3aa6d4e865a49503cda800
mamadfar <m.sabah.farhadi@gmail.com>
2025-09-21 16:41:46 +0200

    #update: wrap up oop

--- 4.oop/2.static/color.js
+++ 4.oop/2.static/color.js
@@ -1,10 +1,55 @@
 import util from "util";
 
-class Color {
+export default class Color {
+  #r = 0;
+  #g = 0;
+  #b = 0;
   constructor(r = 0, g = 0, b = 0) {
     this.r = r;
     this.g = g;
     this.b = b;
+  }
+
+  static fromHex(hex) {
+    const regex = new RegExp("^[0-9a-f]{6}$", "i");
+    if (!regex.test(hex)) {
+      throw new Error("Invalid hex color: " + hex);
+    }
+    return new Color(
+      parseInt(hex.slice(0, 2), 16), //* 16 means hexadecimal and it's base 16 and it will convert to base 10
+      parseInt(hex.slice(2, 4), 16),
+      parseInt(hex.slice(4, 6), 16)
+    );
+  }
+
+  set r(value) {
+    if (value < 0 || value > 255) {
+      throw new RangeError("Red value must be between 0 and 255");
+    }
+    this.#r = parseInt(value);
+  }
+
+  get r() {
+    return this.#r;
+  }
+
+  set g(value) {
+    if (value < 0 || value > 255) {
+      throw new RangeError("Green value must be between 0 and 255");
+    }
+    this.#g = parseInt(value);
+  }
+  get g() {
+    return this.#g;
+  }
+  set b(value) {
+    if (value < 0 || value > 255) {
+      throw new RangeError("Blue value must be between 0 and 255");
+    }
+    this.#b = parseInt(value);
+  }
+  get b() {
+    return this.#b;
   }
 
   static fromHex(hex) {
@@ -24,7 +69,7 @@ class Color {
   }
 
   [util.inspect.custom]() {
-    return `rgb(${this.r}, ${this.g}, ${this.b}) - Inspect custom method`;
+    return `rgb(${this.r}, ${this.g}, ${this.b}) - util.inspect custom method`;
   }
 }
 
--- 4.oop/2.static/private.js
+++ 4.oop/2.static/private.js
@@ -3,32 +3,48 @@ class Color {
   #r = 0;
   #g = 0;
   #b = 0;
+  //? Public field
+  p = 2;
+  static s = 3; //? Static field
+  static #t = 4; //? Static private field
 
   constructor(r = 0, g = 0, b = 0) {
     this.#r = r;
     this.#g = g;
     this.#b = b;
+    this.p = 5; //? This is called Property
     Object.seal(this);
   }
 
   //? Setter and Getter must have the same name
-  set red(value) {
+  set r(value) {
     if (value < 0 || value > 255) {
       throw new RangeError("Red value must be between 0 and 255");
     }
     this.#r = parseInt(value);
   }
 
-  get red() {
+  get r() {
     return this.#r;
+  }
+  #reset() {
+    this.#r = 0;
+    this.#g = 0;
+    this.#b = 0;
+  }
+  //? Static method to access static private field
+  static getT() {
+    return this.#t;
   }
 }
 
 const c1 = new Color(10, 20, 30);
 
-// c1.red = 500;
-c1.red = 200;
-console.log(c1.red);
+// c1.r = 500;
+c1.r = 200;
+console.log(c1.r);
 c1.r = 100;
 console.log(c1.r);
 console.log(c1);
+console.log(Color.s);
+console.log(Color.getT());
